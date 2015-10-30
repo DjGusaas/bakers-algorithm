d750ebd55c969125249e55b79864c604049590ba
DjGusaas <gusaa004@morris.umn.edu>
2015-10-29 19:59:56 -0500

    sleep_costumers and handle_things mostly, having issue with the pids of customers

--- bakers.exs
+++ bakers.exs
@@ -1,34 +1,40 @@
-# Bailey && Dalton
+# Bailey && Dalton && Dillon
 
 defmodule Customer do
-  def print_customer(n) do
-		IO.puts "Customer #{n} arrived at store."
-	#	:timer.sleep(1000)
+  def sleep_customer(n, pid) do
+  #    IO.puts "CPID: #{cpid}"
+    IO.puts "Customer #{n} arrived at store."
+    :timer.sleep(Randomize.random(10000))
+    send(pid, {:test, "test"})
 	end
 
-	def make_customers(0), do: "Finished creating customers."
-	def make_customers(n) when n > 0 do
-		spawn(__MODULE__, :print_customer, [n])
-		#send
-		make_customers(n-1)
-		#receive do
-			#{:sleep } -> this-customer.sleep(:random.uniform()
-			#{:line
-			# sleep for :random.uniform(1000)
-			# fib :random.uniform(40)
-			# make-customers(n-1)
-		#end
-	end
+	def make_customers(0, _), do: "Finished creating customers."
+	def make_customers(n, pid) when n > 0 do
+		spawn(__MODULE__, :sleep_customer, [n, pid])
+    IO.puts "#{self()}"
 
-	#def customer(n) do
-	#	IO.puts "n #{n}}
-	#end
+		make_customers(n-1, pid)
+	end
 end
 
 defmodule Server do
-
+		
 end
 
 defmodule Manager do
+	def handle_things do
+		receive	do
+			{:test, sender} -> IO.puts "Customer #{sender} received"
+			#{:help, sender} customer
+			#{:ready, sender} server
+      handle_things
+		end
+	end
+end
 
+defmodule Randomize do
+  def random(number) do
+    :random.seed(:erlang.now())
+    :random.uniform(number)
+   end
 end
