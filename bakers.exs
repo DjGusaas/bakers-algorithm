e79f6a4915a671605932a87c5ec80d644c326cdd
DjGusaas <gusaa004@morris.umn.edu>
2015-11-03 00:14:57 -0600

    Messed with formatting and loop in Servers

--- bakers.exs
+++ bakers.exs
@@ -1,5 +1,13 @@
 # Bailey && Dalton && Dillon
 
+defmodule Main do
+  def start(cst) do
+    pid = spawn(&Manager.handle_things/0)
+    Customer.make_customers(cst, pid)
+    #Server.make_servers(svr, pid)
+  end
+end
+
 defmodule Customer do
   def sleep_customer(n, pid) do
     IO.puts "Customer #{n} arrived at store."
@@ -8,21 +16,28 @@ defmodule Customer do
     send(pid, {:help, n, self(), fib})
 	end
 
-  def make_customers(0, _), do: "Finished making customers"
+  def make_customers(0, _), do: "Customers created"
 	def make_customers(n, pid) when n > 0 do 
     spawn(__MODULE__, :sleep_customer, [n, pid])
 
 		make_customers(n-1, pid)
 	end
+
+  def loop do
+    receive do
+      {:loop, sender_num} ->
+        IO.puts "Yay! Customer #{sender_num}"
+    end
+  end
 end
 
 defmodule Server do
   def send_server(n, pid) do
-    IO.puts "Sending server #{n}."
+    IO.puts "Server #{n} is getting ready"
     send(pid, {:ready, n, self()})
   end
 
-	def make_servers(0, _), do: "Finished making servers"
+	def make_servers(0, _), do: "Servers created"
   def make_servers(n, pid) when n > 0 do
     spawn(__MODULE__, :send_server, [n, pid])
 
@@ -33,19 +48,13 @@ end
 defmodule Manager do
 	def handle_things do
 		receive	do
-			{:help, sender_id, sender, fib} -> IO.puts "Customer #{sender_id} received, fib: #{fib}"
-			{:ready, sender_id, sender} -> IO.inspect sender #"Server #{sender} received"
+			{:help, sender_num, pid, fib} ->
+		    send(pid, {:loop, sender_num})
+      {:ready, sender_num, pid} ->
+        IO.inspect pid
 		end
     handle_things
 	end
-
-  def customer_line(pid, fib) do
-    if 1==1 do # if a server is open
-            
-    else
-      customer_line(pid, fib)
-    end
-  end
 end
 
 defmodule Randomize do
@@ -53,4 +62,8 @@ defmodule Randomize do
     :random.seed(:erlang.now())
     :random.uniform(number)
    end
+end
+
+defmodule Fib do
+
 end
